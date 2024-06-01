30b6205dec087deb434380fb007e2799555461ac
Bs314 <104457850+Bs314@users.noreply.github.com>
2024-06-01 18:42:27 +0300

    touch control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,13 @@ public class Player : MonoBehaviour
     [SerializeField] Transform rightLimit;
     [SerializeField] Transform leftLimit;
     [SerializeField] GameObject button;
+    [SerializeField] GameObject leftButton;
+    [SerializeField] GameObject rightButton;
+
     float horizontalMove;
     bool playerAlive = true;
+    bool buttonPressed = false;
+    float moveInfoFromButton;
     void Start()
     {
         Player abc = GetComponent<Player>();
@@ -20,10 +25,12 @@ public class Player : MonoBehaviour
     
     void Update()
     {
+
         if(playerAlive)
         {
             Move();
         }
+
         
         
     }
@@ -33,8 +40,14 @@ public class Player : MonoBehaviour
         float limLeft = leftLimit.position.x;
         float limRight = rightLimit.position.x;
         float playerPosition = transform.position.x;
-
-        horizontalMove = Input.GetAxis("Horizontal");   
+        if(buttonPressed)
+        {
+            horizontalMove = moveInfoFromButton;
+        }
+        else{
+            horizontalMove = Input.GetAxis("Horizontal");
+        }
+           
         if((playerPosition > limLeft && horizontalMove < 0) || (playerPosition < limRight && horizontalMove > 0))
         {
             transform.Translate(new Vector3(horizontalMove * moveSpeed * Time.deltaTime,0,0));
@@ -47,6 +60,8 @@ public class Player : MonoBehaviour
             Debug.Log("Ball");
             GetComponent<Rigidbody>().freezeRotation = false;
             playerAlive = false;
+            rightButton.SetActive(false);
+            leftButton.SetActive(false);
             button.SetActive(true);
         }
     }
@@ -56,6 +71,25 @@ public class Player : MonoBehaviour
 
     public bool getPlayerStatus()
     {
+        
         return playerAlive;
     }
+
+    public void GoRight(){
+        buttonPressed = true;
+        moveInfoFromButton = 1;
+    }
+
+    public void GoLeft(){
+        buttonPressed = true;
+        moveInfoFromButton = -1;
+    }
+
+    public void ButtonFree()
+    {
+        moveInfoFromButton = 0;
+        buttonPressed = false;
+    }
+
+
 }
